4505b84efe48466e2c048cafe1e2797af32520bb
Vladimir Migashko <migashko@gmail.com>
2019-10-17 03:41:08 +0300

    [wip] mvc initial

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <chrono>
 
 #include <memory>
+#include <functional>
+#include <iostream>
 
 using namespace std::chrono;
 
@@ -12,6 +14,14 @@ const int W = 1200;
 const int H = 800;
 
 float DEGTORAD = 0.017453f;
+
+struct position
+{
+  float x = 0.0;
+  float y = 0.0;
+  float a = 0.0; // угол
+};
+
 
 class animator
 {
@@ -30,14 +40,14 @@ public:
     _sprite.setOrigin(w/2,h/2);
     _sprite.setTextureRect(_frames[0]);
   }
-  
+
   void update()
   {
     _current_frame += _speed;
     int n = _frames.size();
     if ( _current_frame >= n )
       _current_frame -= n;
-     
+
     if ( n>0 )
       _sprite.setTextureRect( _frames[ static_cast<int>(_current_frame)] );
   }
@@ -46,14 +56,14 @@ public:
   {
     return _current_frame + _speed >= _frames.size();
   }
-   
-  void draw(sf::RenderWindow &app, float x, float y, float angle)
+
+  void draw(sf::RenderWindow &app, const position& pos)
   {
-    _sprite.setPosition(x,y);
-    _sprite.setRotation(angle+90);
+    _sprite.setPosition(pos.x, pos.y);
+    _sprite.setRotation(pos.a+90);
     app.draw(_sprite);
   }
-   
+
 private:
    float _current_frame = 0.0;
    float _speed = 0.0;
@@ -63,136 +73,516 @@ private:
 };
 
 
-class Entity
+namespace model{
+
+class imodel
 {
-   public:
-   float x,y,dx,dy,R,angle;
-   bool life = 1;
-   std::string name;
-   animator anim;
+public:
+  virtual ~imodel() {}
+  virtual void update() = 0;
+  virtual position get_position() const = 0;
+  virtual void set_position( const position& p) = 0;
+  virtual position get_delta() const = 0;
+  virtual float get_radius() const = 0;
+  virtual void rotate(float dr) = 0;
+  virtual void thrust(bool value) = 0;
+  virtual bool is_thrust() = 0;
+  virtual void set_delta( const position& p) = 0;
+  virtual bool is_life() const= 0;
 
-   Entity()
-   {
-     life=1;
+  virtual void kill(bool value)= 0;
+};
+
+class entity: public imodel
+{
+public:
+  typedef std::shared_ptr<entity> ptr;
+  virtual ~entity(){}
+
+  entity(const position& pos,int radius=1)
+    : _pos(pos)
+    , _radius(radius)
+  { }
+
+  virtual position get_position() const
+  {
+    return _pos;
+  }
+
+  virtual position get_delta() const
+  {
+    return _delta;
+  }
+
+  virtual float get_radius() const
+  {
+    return _radius;
+  }
+
+  virtual void rotate(float da)
+  {
+    _pos.a += da;
+  }
+
+  virtual void thrust(bool value)
+  {
+    _thrust = value;
+  }
+
+  virtual bool is_thrust()
+  {
+    return _thrust;
+  }
+
+  virtual bool is_life() const
+  {
+    return _life;
+  }
+
+  virtual void kill(bool value)
+  {
+    _life = !value;
+  }
+
+  virtual void set_delta( const position& p)
+  {
+    _delta = p;
+  }
+
+  virtual void set_position( const position& p)
+  {
+    _pos = p;
+  }
+
+  virtual void update() override
+  {
+  }
+
+  bool is_collide(const entity& other)
+  {
+    position a = this->get_position();
+    float ar = this->get_radius();
+    position b = other.get_position();
+    float br = other.get_radius();
+    return (b.x - a.x)*(b.x - a.x)+
+           (b.y - a.y)*(b.y - a.y)<
+           (ar + br)*(ar + br);
+  }
+
+protected:
+  position _pos;
+  position _delta;
+  int _radius = 0;
+  bool _life = true;
+  bool _thrust = false;
+};
+
+class asteroid: public entity
+{
+public:
+  typedef std::shared_ptr<asteroid> ptr;
+
+  asteroid(const position& pos,int radius=1)
+    : entity(pos, radius)
+  {
+    _delta.x=rand()%8-4;
+    _delta.y=rand()%8-4;
+  }
+
+  virtual void update() override
+  {
+    _pos.x+=_delta.x;
+    _pos.y+=_delta.y;
+
+    if (_pos.x>W) _pos.x=0;  if ( _pos.x<0 ) _pos.x = W;
+    if (_pos.y>H) _pos.y=0;  if ( _pos.y<0 ) _pos.y = H;
+  }
+};
+
+class bullet: public entity
+{
+public:
+  typedef std::shared_ptr<bullet> ptr;
+  bullet(const position& pos,int radius=1)
+    : entity(pos, radius)
+  {}
+
+  bullet(const position& pos, const position& delta, int radius=1)
+    : entity(pos, radius)
+    , start_dx(delta.x)
+    , start_dy(delta.y)
+  {}
+
+  virtual void update() override
+  {
+    _delta.x=cos(_pos.a*DEGTORAD)*6 + start_dx;
+    _delta.y=sin(_pos.a*DEGTORAD)*6 + start_dy;
+     // "Покачивание" пуль
+     //angle+=rand()%7-3;  /*try this*/
+     //angle+=rand()%20-10;  /*try this*/
+    _pos.x+=_delta.x;
+    _pos.y+=_delta.y;
+
+     if ( _pos.x>W || _pos.x<0 || _pos.y>H || _pos.y<0 )
+       _life=false;
    }
 
-   void settings(const animator &a,int X,int Y,float Angle=0,int radius=1)
-   {
-     anim = a;
-     x=X; y=Y;
-     angle = Angle;
-     R = radius;
+   float start_dx = 0;
+   float start_dy = 0;
+};
+
+class player: public entity
+{
+public:
+  typedef std::shared_ptr<player> ptr;
+
+  player(const position& pos,int radius=1)
+    : entity(pos, radius)
+  {}
+
+  virtual void update() override
+  {
+    if ( _thrust )
+    {
+      _delta.x+=cos(_pos.a*DEGTORAD)*0.2;
+      _delta.y+=sin(_pos.a*DEGTORAD)*0.2;
+    }
+    else
+    {
+      /*_delta.x*=0.99;
+      _delta.y*=0.99;*/
+    }
+
+    int maxSpeed=15;
+    float speed = sqrt(_delta.x*_delta.x+_delta.y*_delta.y);
+    if ( speed > maxSpeed )
+    {
+      _delta.x *= maxSpeed/speed;
+      _delta.y *= maxSpeed/speed;
+    }
+
+    _pos.x += _delta.x;
+    _pos.y += _delta.y;
+
+    if ( _pos.x > W ) _pos.x=0;
+    if ( _pos.x< 0 ) _pos.x=W;
+    if ( _pos.y>H ) _pos.y=0;
+    if ( _pos.y<0 ) _pos.y=H;
    }
 
-   virtual void update(){};
+  void fire(bool value)
+  {
+    _fire = value;
+  }
+
+  bool is_fire() const
+  {
+    return _fire;
+  }
+
+  bool _fire = false;
+};
+
+class engine
+{
+  typedef std::list<asteroid::ptr> asteroid_list;
+  typedef std::list<bullet::ptr> bullet_list;
+  typedef std::list<entity::ptr> explosion_list;
+
+public:
+
+  void initialize()
+  {
+     _last_fire = high_resolution_clock::now();
+    _player = this->create_player();
+    for(int i=0;i<15;i++)
+    {
+      auto a = create_asteroid();
+      _new_asteroids.push_back(a);
+      _asteroids.push_back(a);
+    }
+  }
+
+  void update()
+  {
+    std::cout << "asteroids: " << _asteroids.size() << " " << _new_asteroids.size() << std::endl;
+    std::cout << "bullets: " << _bullets.size() << " " << _new_bullets.size() << std::endl;
+    _player->update();
+    this->update_bullets();
+    this->update_asteroids();
+    this->update_explosions();
+    this->fire_bullets();
+    this->collide_bullets();
+    this->create_random_asteroid_or_not();
+  }
+
+  void update_bullets()
+  {
+    auto itr = _bullets.begin();
+    for ( ; itr != _bullets.end() ; )
+    {
+      (*itr)->update();
+      if ( !(*itr)->is_life() )
+        _bullets.erase(itr++);
+      else
+        ++itr;
+    }
+  }
+
+  void update_asteroids()
+  {
+    auto itr = _asteroids.begin();
+    for ( ; itr != _asteroids.end() ; )
+    {
+      (*itr)->update();
+      if ( !(*itr)->is_life() )
+        _asteroids.erase(itr++);
+      else
+        ++itr;
+    }
+  }
+
+  void update_explosions()
+  {
+    auto itr = _explosions.begin();
+    for ( ; itr != _explosions.end() ; )
+    {
+      (*itr)->update();
+      if ( !(*itr)->is_life() )
+        _explosions.erase(itr++);
+      else
+        ++itr;
+    }
+  }
+
+  void fire_bullets()
+  {
+    if ( _player->is_fire() )
+    {
+      auto now = high_resolution_clock::now();
+      if ( /*75000*/ /*35000*/ 10000 < duration_cast<std::chrono::microseconds>(now - _last_fire).count() )
+      {
+        _last_fire = now;
+        auto b = create_bullet();
+        _new_bullets.push_back(b);
+        _bullets.push_back(b);
+      }
+    }
+  }
+
+  void collide_bullets()
+  {
+    bool collide_player = false;
+    for (const auto& a : _asteroids )
+    {
+      if ( !collide_player && a->is_collide(*_player) )
+      {
+        collide_player = true;
+        a->kill(true);
+        auto b = create_explosion(a->get_position());
+        _new_explosions.push_back(b);
+        _explosions.push_back(b);
+        _player->set_position(position{W/2,H/2,0});
+        _player->set_delta(position{0,0,0});
+      }
+
+      for (const auto& b : _bullets)
+      {
+        if ( a->is_collide(*b) )
+        {
+          a->kill(true);
+          b->kill(true);
+          auto b = create_explosion(a->get_position());
+          _new_explosions.push_back(b);
+          _explosions.push_back(b);
+
+          if ( a->get_radius()==25 )
+          {
+            for(int i=0;i<8;i++)
+            {
+              auto sa = create_small_asteroid(a->get_position());
+              _new_asteroids.push_back(sa);
+              _asteroids.push_back(sa);
+            }
+          }
+
+        }
+      }
+    }
+  }
+
+
+  void create_random_asteroid_or_not()
+  {
+    if ( rand()%/*1*/50==0 )
+    {
+      auto a = create_asteroid();
+      _new_asteroids.push_back(a);
+      _asteroids.push_back(a);
+    }
+  }
+
+
+  asteroid::ptr detach_new_asteroid()
+  {
+    if ( _new_asteroids.empty() )
+      return nullptr;
+    asteroid::ptr a = _new_asteroids.front();
+    _new_asteroids.pop_front();
+    return a;
+  }
+
+  bullet::ptr detach_new_bullet()
+  {
+    if ( _new_bullets.empty() )
+      return nullptr;
+    bullet::ptr a = _new_bullets.front();
+    _new_bullets.pop_front();
+    return a;
+  }
+
+  entity::ptr detach_new_explosions()
+  {
+    if ( _new_explosions.empty() )
+      return nullptr;
+    entity::ptr a = _new_explosions.front();
+    _new_explosions.pop_front();
+    return a;
+  }
+
+  player::ptr get_player()
+  {
+    return _player;
+  }
+
+  static player::ptr create_player()
+  {
+    return std::make_shared<player>( position{200,200,0},20 );
+  }
+
+  static asteroid::ptr create_asteroid()
+  {
+    return std::make_shared<asteroid>( position{ 0, float(rand()%H), float(rand()%360)}, 25);
+  }
+
+  static asteroid::ptr create_small_asteroid(const position& pos)
+  {
+    return std::make_shared<asteroid>( position{ pos.x, pos.y, float(rand()%360)}, 15);
+  }
+
+  // TODO: сделать класс explosion
+  static entity::ptr create_explosion(const position& pos)
+  {
+    return std::make_shared<entity>( pos, 0);
+  }
+
+
+  bullet::ptr create_bullet()
+  {
+    if ( _player == nullptr )
+      return nullptr;
+    return std::make_shared<bullet>( _player->get_position(), _player->get_delta(), 25);
+  }
+
+private:
+  player::ptr _player;
+  asteroid_list _asteroids;
+  bullet_list _bullets;
+  explosion_list _explosions;
+  asteroid_list _new_asteroids;
+  bullet_list _new_bullets;
+  explosion_list _new_explosions;
+  high_resolution_clock::time_point _last_fire;
+};
+
+}
+
+class entity
+{
+public:
+  typedef std::shared_ptr<entity> ptr;
+  //float x,y,dx,dy,R,angle;
+  std::string name;
+  animator anim;
+
+  entity()
+  {
+  }
+
+  void settings(const animator &a, std::shared_ptr<model::imodel> m)
+  {
+    anim = a;
+    _model = m;
+  }
+
+   virtual void update()
+   {
+      _model->update();
+    }
 
    void draw(sf::RenderWindow &app)
    {
-     anim.draw(app, x, y, angle);
-     
+     anim.draw(app, _model->get_position());
+      /*
      sf::CircleShape circle(R);
      circle.setFillColor(sf::Color(255,0,0,170));
      circle.setPosition(x,y);
      circle.setOrigin(R,R);
      //app.draw(circle);
+      */
    }
 
-   virtual ~Entity(){};
+   virtual ~entity(){};
+
+  std::shared_ptr<model::imodel> get_model()
+  {
+    return _model;
+  }
+private:
+  std::shared_ptr<model::imodel> _model;
 };
 
 
-class asteroid: public Entity
+class asteroid: public entity
 {
-   public:
+public:
    asteroid()
    {
-     dx=rand()%8-4;
-     dy=rand()%8-4;
      name="asteroid";
    }
-
-   void update()
-   {
-     x+=dx;
-     y+=dy;
-
-     if (x>W) x=0;  if (x<0) x=W;
-     if (y>H) y=0;  if (y<0) y=H;
-   }
-
 };
 
 
-class bullet: public Entity
+class bullet: public entity
 {
-   public:
+public:
    bullet()
    {
      name="bullet";
    }
-
-   void  update()
-   {
-     dx=cos(angle*DEGTORAD)*6 + start_dx;
-     dy=sin(angle*DEGTORAD)*6 + start_dy;
-     // "Покачивание" пуль
-     //angle+=rand()%7-3;  /*try this*/
-     //angle+=rand()%20-10;  /*try this*/
-     x+=dx;
-     y+=dy;
-
-     if (x>W || x<0 || y>H || y<0) life=0;
-   }
-   
-   float start_dx = 0;
-   float start_dy = 0;
 };
 
 
-class player: public Entity
+class player: public entity
 {
-   public:
-   bool thrust;
-
+public:
    player()
    {
      name="player";
    }
-
-   void update()
-   {
-     if (thrust)
-     {
-        dx+=cos(angle*DEGTORAD)*0.2;
-        dy+=sin(angle*DEGTORAD)*0.2; 
-    }
-    else
-    {
-      dx*=0.99;
-      dy*=0.99; 
-    }
-
-    int maxSpeed=15;
-    float speed = sqrt(dx*dx+dy*dy);
-    if (speed>maxSpeed)
-     { dx *= maxSpeed/speed;
-       dy *= maxSpeed/speed; }
-
-    x+=dx;
-    y+=dy;
-
-    if (x>W) x=0; if (x<0) x=W;
-    if (y>H) y=0; if (y<0) y=H;
-   }
-
 };
 
 
-bool isCollide(Entity *a,Entity *b)
+bool isCollide(entity *ma, entity *mb)
 {
-  return (b->x - a->x)*(b->x - a->x)+
-         (b->y - a->y)*(b->y - a->y)<
-         (a->R + b->R)*(a->R + b->R);
+  position a = ma->get_model()->get_position();
+  float ar = ma->get_model()->get_radius();
+  position b = mb->get_model()->get_position();
+  float br = mb->get_model()->get_radius();
+
+  return (b.x - a.x)*(b.x - a.x)+
+         (b.y - a.y)*(b.y - a.y)<
+         (ar + br)*(ar + br);
 }
 
 
@@ -226,21 +616,22 @@ int main()
     auto sExplosion_ship=std::make_shared<animator>(t7, 0,0,192,192, 64, 0.5);
 
 
-    std::list<Entity*> entities;
+    std::list<entity*> entities;
 
-    for(int i=0;i<15;i++)
+    model::engine engine;
+    engine.initialize();
+
+    while ( auto am = engine.detach_new_asteroid() )
     {
       asteroid *a = new asteroid();
-      a->settings(*sRock, rand()%W, rand()%H, rand()%360, 25);
+      a->settings(*sRock, am );
       entities.push_back(a);
     }
 
     player *p = new player();
-    p->settings(*sPlayer,200,200,0,20);
+    p->settings(*sPlayer, engine.get_player() );
     entities.push_back(p);
 
-    auto gun_time = high_resolution_clock::now();
-    
     /////main loop/////
     while (app.isOpen())
     {
@@ -251,93 +642,65 @@ int main()
           app.close();
       }
 
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+      engine.get_player()->fire( sf::Keyboard::isKeyPressed(sf::Keyboard::Space) );
+
+      if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) p->get_model()->rotate(3);
+      if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Left))  p->get_model()->rotate(-3);
+      if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) p->get_model()->thrust(true);
+      else p->get_model()->thrust(false);
+
+      /////////////////////////////
+      engine.update();
+      /////////////////////////////
+
+      while ( auto nb = engine.detach_new_bullet() )
       {
-        auto now = high_resolution_clock::now();
-        if ( /*75000*/ 35000 < duration_cast<std::chrono::microseconds>(now - gun_time).count() )
-        {
-          gun_time = now;
-          bullet *b = new bullet();
-          b->settings(*sBullet,p->x,p->y,p->angle,10);
-          b->start_dx = p->dx;
-          b->start_dy = p->dy;
-          entities.push_back(b);
-        }
+        bullet *b = new bullet();
+        b->settings(*sBullet, nb);
+        entities.push_back(b);
       }
 
-      if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) p->angle+=3;
-      if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Left))  p->angle-=3;
-      if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) p->thrust=true;
-      else p->thrust=false;
+      std::cout << "entities " << entities.size() << std::endl;
 
-      for(auto a:entities)
-      {
-        for(auto b:entities)
-        {
-          if (a->name=="asteroid" && b->name=="bullet")
-          {
-            if ( isCollide(a,b) )
-            {
-              a->life=false;
-              b->life=false;
-
-              Entity *e = new Entity();
-              e->settings(*sExplosion,a->x,a->y);
-              e->name="explosion";
-              entities.push_back(e);
-
-              for(int i=0;i<2;i++)
-              {
-                if (a->R==15) continue;
-                Entity *e = new asteroid();
-                e->settings(*sRock_small,a->x,a->y,rand()%360,15);
-                entities.push_back(e);
-              }
-            }
-          }
-
-          if (a->name=="player" && b->name=="asteroid")
-          {
-            if ( isCollide(a,b) )
-            {
-              b->life=false;
-
-              Entity *e = new Entity();
-              e->settings(*sExplosion_ship,a->x,a->y);
-              e->name="explosion";
-              entities.push_back(e);
-
-              p->settings(*sPlayer,W/2,H/2,0,20);
-              p->dx=0; p->dy=0;
-            }
-          }
-        }
-      }
-
-      if (p->thrust)  p->anim = *sPlayer_go;
+      if (p->get_model()->is_thrust() )  p->anim = *sPlayer_go;
       else   p->anim = *sPlayer;
 
       for(auto e:entities)
       {
         if (e->name=="explosion")
-          if (e->anim.isEnd()) e->life=0;
+          if (e->anim.isEnd()) e->get_model()->kill(true);
       }
 
-      if (rand()%150==0)
+      while ( auto am = engine.detach_new_asteroid() )
       {
         asteroid *a = new asteroid();
-        a->settings(*sRock, 0,rand()%H, rand()%360, 25);
+        if ( am->get_radius() > 15 )
+          a->settings(*sRock, am );
+        else
+          a->settings(*sRock_small, am );
         entities.push_back(a);
+      }
+
+      while ( auto em = engine.detach_new_explosions() )
+      {
+        entity *e = new entity();
+        e->settings(*sExplosion, em );
+        e->name="explosion";
+        entities.push_back(e);
       }
 
       for(auto i=entities.begin();i!=entities.end();)
       {
-        Entity *e = *i;
-        e->update();
+        entity *e = *i;
         e->anim.update();
 
-        if (e->life==false) {i=entities.erase(i); delete e;}
-        else i++;
+        if ( !e->get_model()->is_life() )
+        {
+          i=entities.erase(i);
+          delete e;
+        }
+        else
+          i++;
       }
 
       //////draw//////
@@ -345,6 +708,6 @@ int main()
       for(auto i:entities) i->draw(app);
       app.display();
     }
-  
+
     return 0;
 }
